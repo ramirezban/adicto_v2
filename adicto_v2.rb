6585c810d13f07595f422d6843e8e394dbf3f805
Esteban Ramirez <ramirezesteban91@gmail.com>
2020-10-26 16:28:09 -0300

    adicto v2

---
+++ adicto_v2.rb
@@ -0,0 +1,18 @@
+def scan_addicts2(array)
+  results = []
+  n = array.count
+  n.times do |i|
+    if array[i] >= 180
+      results.push 'mal'
+    elsif array[i] >= 90
+
+      results.push 'mejorable'
+    else
+      results.push 'bien'
+    end
+  end
+  results # cuidado con el retorno, times retorna la cuenta
+end
+
+print scan_addicts2([120, 90, 600, 30, 90, 10, 200, 180, 500])
+["mejorable", "mejorable", "mal", "bien", "mejorable", "bien", "mal", "mal", "mal"]
